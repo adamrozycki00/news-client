da75811a738cffbbf269ae3b376e5a8890b9c04f
Adam Rozycki <adamrozycki00@gmail.com>
2020-03-28 15:10:20 +0100

    View

--- src/main/java/pl/adaroz/springboot2/homework5/controller/NewsController.java
+++ src/main/java/pl/adaroz/springboot2/homework5/controller/NewsController.java
@@ -1,20 +1,29 @@
 package pl.adaroz.springboot2.homework5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 import pl.adaroz.springboot2.homework5.model.Article;
 import pl.adaroz.springboot2.homework5.model.News;
 
+import java.util.List;
+
 @Controller
 public class NewsController {
 
-    public NewsController() {
+    private List<Article> getNews() {
         RestTemplate restTemplate = new RestTemplate();
         String apikey = "&apikey=6c863c080bc7468d9de672762381fc4a";
         News news = restTemplate.getForObject("http://newsapi.org/v2/top-headlines?country=us" + apikey,
                 News.class);
-        for (Article article : news.getArticles())
-            System.out.println(article);
+        return news.getArticles();
+    }
+
+    @GetMapping("/news")
+    public String news(Model model) {
+        model.addAttribute("news", getNews());
+        return "news";
     }
 
 }
